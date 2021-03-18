efb242c3b9a3b7ea6d6879aba57455a7d386169b
MJKKK <ddmn22@naver.com>
2021-03-18 18:19:11 +0900

    Chapter25. 스레드

---
+++ Chapter25/src/Ex05_MultiThread.java
@@ -0,0 +1,39 @@
+
+public class Ex05_MultiThread {
+
+	public static void main(String[] args) {
+
+		Runnable task1 = () ->{
+			try
+			{
+				for(int i = 0; i < 20; i = i+2)			// 20미만 짝수 출력
+				{
+					System.out.print(i + " ");
+					Thread.sleep(1000);					// 1000밀리세컨드(1초) 쉼
+				}
+			}
+			catch(InterruptedException e) {}
+			
+		};
+
+		Runnable task2 = () ->{
+			try
+			{
+				for(int i = 9; i > 0; i--)				// 10미만 홀수 출력
+				{
+					System.out.print("(" + i + ") ");
+					Thread.sleep(500);					// 500밀리세컨드 쉼
+				}
+			}
+			catch(InterruptedException e) {}
+			
+		};
+		
+		Thread t1 = new Thread(task1);
+		Thread t2 = new Thread(task2);
+		
+		t1.start();
+		t2.start();
+	}
+
+}
